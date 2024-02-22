4f00db53ed455447587502f478aa75eae21c6e7d
shashank.s <148774696+shashankssvi@users.noreply.github.com>
2024-02-22 09:05:57 +0530

    Update numpy

--- numpy/9.py
+++ numpy/9.py
@@ -1,4 +1,17 @@
 import numpy as np
 n1 = np.array([1, 2, 3, 4, 5], dtype='S')
 print(n1.dtype)
-
+'''
+datatype:-
+i - integer
+b - boolean
+u - unsigned integer
+f - float
+c - complex float
+m - timedelta
+M - datetime
+O - object
+S - string
+U - unicode string
+V - fixed chunk of memory for other type ( void )
+'''
